d73cdb19bf2a8b9e48d971aa002cc93b881aa1be
Eugene Gusev <gagoose95@gmail.com>
2021-08-13 00:41:43 +0700

    Added project structure, swagger, database, simple order, and simple products

---
+++ DataAccess.Interfaces/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Interfaces
+{
+    public interface IDbContext
+    {
+        DbSet<Order> Orders { get; }
+        DbSet<Product> Products { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken token = default);
+    }
+}
